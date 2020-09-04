cecd06bd5942a767605b0ae26d452cdcf00ff7f6
ksh <hyun031916@gmail.com>
2020-09-05 02:40:14 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/view_pictures_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.view_pictures_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
