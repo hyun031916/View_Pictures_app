784ed347931759558048792c6a2eb382979eed71
ksh <hyun031916@gmail.com>
2020-09-09 00:14:48 +0900

    #2 Java 코드 1 전역변수 선언

--- app/src/main/java/com/cookandroid/view_pictures_app/MainActivity.java
+++ app/src/main/java/com/cookandroid/view_pictures_app/MainActivity.java
@@ -3,12 +3,30 @@ package com.cookandroid.view_pictures_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView text1, text2;
+    CheckBox chkAgree;
+    RadioGroup rGroup1;
+    RadioButton rdoDog, rdoCat, rdoRabbit;
+    Button btnOk;
+    ImageView imgPet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("애완동물 사진 보기");
+
+        text1 = (TextView)findViewById(R.id.Text1);
+        chkAgree = (CheckBox)findViewById(R.id.ChkAgree);
+
+        text2 = (TextView)findViewById(R.id.Text2);
     }
 }
